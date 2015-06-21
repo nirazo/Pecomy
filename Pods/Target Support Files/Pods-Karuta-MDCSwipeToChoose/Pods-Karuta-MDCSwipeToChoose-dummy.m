8411ccc0761982c9dea932b9db646c0798535c50
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2015-06-22 00:40:08 +0900

    cocoapods導入

---
+++ Pods/Target Support Files/Pods-Karuta-MDCSwipeToChoose/Pods-Karuta-MDCSwipeToChoose-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karuta_MDCSwipeToChoose : NSObject
+@end
+@implementation PodsDummy_Pods_Karuta_MDCSwipeToChoose
+@end
---
+++ Pods/Target Support Files/Pods-Karuta/Pods-Karuta-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Karuta : NSObject
+@end
+@implementation PodsDummy_Pods_Karuta
+@end
