a6676f62e2f58c84a5233e21fa70299330abcffb
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2016-08-07 19:51:24 +0900

    アプリ名変更

--- Pecomy/Classes/Common/Const.swift
+++ Pecomy/Classes/Common/Const.swift
@@ -94,7 +94,7 @@ struct Const {
     static let WILL_ENTER_FOREGROUND_KEY = "applicationWillEnterForeground"
     
     // GoogleMaps SDKのAPIキー
-    static let GOOGLEMAP_API_KEY = "AIzaSyDoWEe-eYp1z0SaJ64JkQ2TuDzK1YOatmw"
+    static let GOOGLEMAP_API_KEY = "AIzaSyAEXL52glVXfls6JzlsQevrcHUDoJwr5iI"
     
     // ウインドウのサイズ
     static let WindowSize = UIScreen.mainScreen().bounds.size
